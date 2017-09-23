8127eac04ccbab9d88cd2c14e12d61c3b100e3f0
Nick Thomas <nichoth@gmail.com>
2017-09-22 20:20:33 -0700

    do recursive namespacing

--- index.js
+++ index.js
@@ -1,51 +1,13 @@
-var Bus = require('nanobus')
-var inherits = require('inherits')
+module.exports = function namespace (arr, ns) {
+    ns = ns || ''
+    if (Array.isArray(arr)) return arr.reduce(function (acc, name) {
+        acc[name] = ns + '.' + name
+        return acc
+    }, {})
 
-function namespace (evs) {
-    return Object.keys(evs).reduce(function (acc, k) {
-        acc[k] = evs[k].reduce(function (_acc, name) {
-            _acc[name] = k + '.' + name
-            return _acc
-        }, {})
-
+    return Object.keys(arr).reduce(function (acc, k) {
+        acc[k] = namespace(arr[k], ns ? (ns + '.' + k) : k)
         return acc
     }, {})
 }
-
-function Topic (evObj, bus) {
-    if (!(this instanceof Topic)) return new Topic(evObj, bus)
-    Bus.call(this)
-    var self = this
-    this._listeners = []
-    this.parent = bus
-    Object.keys(evObj).forEach(function (k) {
-        function reEmit (data) {
-            self.emit(k, data)
-        }
-        bus.on(evObj[k], reEmit)
-        self._listeners.push([evObj[k], reEmit])
-    })
-}
-inherits(Topic, Bus)
-
-Topic.prototype.close = function () {
-    var self = this
-    this._listeners.forEach(function (pair) {
-        self.parent.removeListener(pair[0], pair[1])
-    })
-    this.removeAllListeners()
-}
-
-function _topic (evs, bus) {
-    if (!bus) return function (_bus) {
-        return _topic(evs, _bus)
-    }
-    return Topic(evs, bus)
-}
-
-module.exports = {
-    Topic: _topic,
-    namespace: namespace
-}
-
 
---
+++ test/index.js
@@ -0,0 +1,40 @@
+var namespace = require('../')
+var test = require('tape')
+
+var input = {
+    events: {
+        update: ['get', 'add', 'delete', 'edit'],
+    },
+
+    foo: {
+        bar: {
+            baz: ['a', 'b', 'c']
+        }
+    }
+}
+
+var expected = {
+    events: {
+        update: {
+            get: 'events.update.get',
+            add: 'events.update.add',
+            delete: 'events.update.delete',
+            edit: 'events.update.edit'
+        }
+    },
+    foo: {
+        bar: {
+            baz: {
+                a: 'foo.bar.baz.a',
+                b: 'foo.bar.baz.b',
+                c: 'foo.bar.baz.c'
+            }
+        }
+    }
+}
+
+test('namespace', function (t) {
+    t.plan(1)
+    t.deepEqual(namespace(input), expected)
+})
+
