da6c7b0af18e039ae897408a7808e612314d9640
Nick Thomas <nichoth@gmail.com>
2017-09-24 12:48:53 -0700

    update example

--- example/index.js
+++ example/index.js
@@ -1,35 +1,16 @@
-var Evs = require('../')
-var namespace = Evs.namespace
-var Topic = Evs.Topic
-var Bus = require('nanobus')
+var namespace = require('../')
 
-var events = namespace({
-    foo: ['a', 'b', 'c'],
-    bar: ['a', 'b', 'c']
-})
+var input = {
+    events: {
+        update: ['get', 'add', 'delete', 'edit'],
+    },
 
-console.log(events)
-// { foo: { a: 'foo.a', b: 'foo.b', c: 'foo.c' },
-//       bar: { a: 'bar.a', b: 'bar.b', c: 'bar.c' } }
+    foo: {
+        bar: {
+            baz: ['a', 'b', 'c']
+        }
+    }
+}
 
-var bus = Bus()
-var topic = Topic(events.foo, bus)
-
-bus.on('*', console.log.bind(console, 'bus'))
-topic.on('*', console.log.bind(console, 'topic'))
-
-bus.emit(events.foo.a, 'hello')
-// topic a hello
-// bus foo.a hello
-
-bus.emit(events.bar.a, 'wooo')
-// bus bar.a wooo
-
-bus.emit(events.foo.b, 'here')
-// topic b here
-// bus foo.b here
-
-topic.close()
-bus.emit(events.foo.a, 'should be closed')
-// bus foo.a should be closed
+console.dir(namespace(input), { depth: null })
 
